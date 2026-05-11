7957798727b6ac25836e14d048fb63f0f0ae1dad
Jiujiu1303 <wimi1113@qq.com>
2026-05-11 19:33:04 +0700

    Update collect.js

--- API/collect.js
+++ API/collect.js
@@ -1,13 +1,20 @@
 export default async function handler(req, res) {
 
   if (req.method !== 'POST') {
+
     return res.status(405).json({
       error: 'Method not allowed'
     });
+
   }
 
-  console.log('收到数据:');
-  console.log(req.body);
+  console.log('========== 收到数据 ==========');
+
+  console.log(
+    JSON.stringify(req.body, null, 2)
+  );
+
+  console.log('==============================');
 
   return res.status(200).json({
     success: true
